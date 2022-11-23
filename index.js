404ab01bedd3c11c25e38a920ce0d6f84422fbe8
Josphat-Kip <josphatkiplimo247@gmail.com>
2022-11-23 08:21:03 +0300

    Jose

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Josphat Kiplimo';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const charact = currentUser.charAt(0);
+const shortGreeting = `Welcome, ${charact}!`;
